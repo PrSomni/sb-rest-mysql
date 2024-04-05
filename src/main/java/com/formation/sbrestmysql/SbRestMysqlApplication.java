7db10e0248369e75cf8f0c2adf3da10620461d30
PrSomni <gabrield@hotmail.fr>
2024-04-05 12:50:36 +0200

    second commit

---
+++ src/main/java/com/formation/sbrestmysql/SbRestMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.formation.sbrestmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/formation/sbrestmysql/SbRestMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation.sbrestmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRestMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
